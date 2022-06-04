e5aef285d26cc5b3e40df50a55fb218e0d1ea759
nitipat21 <nitipat.rstw@gmail.com>
2022-06-04 21:22:25 +0700

    remove token

--- src/Layouts/Instagram.tsx
+++ src/Layouts/Instagram.tsx
@@ -5,23 +5,26 @@ import ImageInstagram3 from '../Assets/Images/instagram3.jpg'
 
 const Instagram:FC = () => {
 
-  
 
 
-  const getMedia =async () => {
-    const data = await fetch(`https://graph.facebook.com/v14.0/17841453247591019?fields=media&access_token=EAAPuCi0VKNABAA8N5Vi4IJXLXZCVNiIR1886MPXzGzU1t9FNaeXxG3WPDZAZB0ZCosTbz9UcgJv4v5aB9K8crBAjX7pIGFPakomx1ILGUiOApf91E77pklwbHhZAOG1puQuaUMUUd0ZB92791n9NpJC6qs5l2DCbgG6ZAtv8HtivFWtYaYroFtQuFRFVakKZBHxK3KAfVwS6fIfViSaZCUhQa`);
+  const getMedia = async () => {
+    const data = await fetch(`https://graph.facebook.com/v14.0/17841453247591019?fields=media&access_token=`);
 
     const instagramPage = await data.json();
 
     const instagramMediaArray = instagramPage.media.data;
     
     const unsolvedPromises = instagramMediaArray.map(async (mediaData:any) => {
-      const data = await fetch(`https://graph.facebook.com/v14.0/${mediaData.id}?fields=media_url&access_token=EAAPuCi0VKNABAA8N5Vi4IJXLXZCVNiIR1886MPXzGzU1t9FNaeXxG3WPDZAZB0ZCosTbz9UcgJv4v5aB9K8crBAjX7pIGFPakomx1ILGUiOApf91E77pklwbHhZAOG1puQuaUMUUd0ZB92791n9NpJC6qs5l2DCbgG6ZAtv8HtivFWtYaYroFtQuFRFVakKZBHxK3KAfVwS6fIfViSaZCUhQa`);
+      const data = await fetch(`https://graph.facebook.com/v14.0/${mediaData.id}?fields=media_url&access_token=`);
+
       return data.json();
     })
 
-    console.log(unsolvedPromises)
+    console.log(unsolvedPromises);
+
     const result = await Promise.all([unsolvedPromises]);
+
+    console.log(result);
     
   }
 
